67e24931850e2087eeaee72ee36fe6598b9dc8b2
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-08-04 13:37:04 -0700

    Mongodb config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
+// Import required modules
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
+// Constants
+const PORT = 8080;
+const MONGO_URL =
+  'mongodb+srv://tanaharaguy:anONHzNZwTE6lTwl@cluster0.h3krndw.mongodb.net/?retryWrites=true&w=majority';
 
+// Initialize Express app
 const app = express();
 
+// Middleware
 app.use(cors({
-   credentials:true,
-}))
-
+  credentials: true,
+}));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+// Connect to MongoDB
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error) => {
+  console.error('🔴 Connection error:', error.message);
+});
+
+// Start the server
 const server = http.createServer(app);
-server.listen(8080, ()=> {
-   console.log('server listening on http://localhost:8080/');
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}/`);
 });
