f973ebdc6437b6c2f0c5c12036ef25d7920d41ed
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-08-06 20:38:50 -0700

    update: users.ts

--- src/db/users.ts
+++ src/db/users.ts
@@ -19,4 +19,7 @@ export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
-export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+export const updateUserById = async (id: string, values: Record<string, any>) => {
+   const updatedUser = await UserModel.findByIdAndUpdate(id, values, { new: true });
+   return updatedUser ? updatedUser.toObject() : null;
+};
